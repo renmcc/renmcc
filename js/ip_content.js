0399db5dacf887866f402b08c783d02a7abf508b
ren_mcc <ren_mcc@foxmail.com>
2023-06-20 03:43:50 +0800

    Site updated: 2023-06-20 03:43:47

--- js/ip_content.js
+++ js/ip_content.js
@@ -30,6 +30,32 @@ function getBrowserInfo() {
 
 var ip_content = document.querySelector(".ip_content");
 
-if (ip_content != null && typeof (returnCitySN) != undefined) {
-    ip_content.innerHTML = '欢迎来自 <span class="p red">' + returnCitySN["cname"] + "</span> 的小伙伴<br>" + "访问IP为： <span class='p cyan'>" + returnCitySN["cip"] + "</span><br>浏览器版本：<span class='p blue'>" + getBrowserInfo() + '</span>';
+// console.log('aaaaaaaaa')
+// const xhr = new XMLHttpRequest();
+// xhr.open('GET','https://whois.pconline.com.cn/ipJson.jsp');
+// xhr.setRequestHeader("Content-type","application/json");
+// xhr.send();
+
+// xhr.onreadystatechange = function() {
+//     // 判断相应状态码200 404 403 401 500
+//     // 2xx 成功
+//     if(xhr.readyState === 4) {
+//         if(xhr.status >= 200 && xhr.status < 300) {
+//             // 处理结果 行 头 空行 体
+//             // 1.相应行
+//             console.log(xhr.status);//状态码
+//             console.log(xhr.statusText);//状态字符串
+//             console.log(xhr.getAllResponseHeaders());//所有响应头
+//             console.log(xhr.response);//响应体
+//         }
+//     }
+// }
+
+
+if (ip_content != null && typeof (ifconfig_io ) != undefined) {
+    ip_content.innerHTML = '欢迎来自 <span class="p red">' + ifconfig_io ["country_code"] + "</span> 的小伙伴<br>" + "访问IP为： <span class='p cyan'>" + ifconfig_io ["ip"] + "</span><br>浏览器版本：<span class='p blue'>" + getBrowserInfo() + '</span>';
 }
+
+
+
+
